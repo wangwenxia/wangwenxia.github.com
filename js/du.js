e7b30f2f4106d162a86e47ebe0892da9a3dd010e
your name <353662497@qq.com>
2016-07-24 18:18:32 +0800

    mobile

---
+++ js/du.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  window.onload=function(){  
+                 changeDivHeight();  
+            }  
+            //当浏览器窗口大小改变时，设置显示内容的高度  
+            window.onresize=function(){  
+                 changeDivHeight();  
+            }  
+            function changeDivHeight(){               
+                var w = document.documentElement.clientWidth;//获取页面可见高度  
+                if (w<768) {
+                  $(".text").css("width",w);
+                  $(".time").css("width",w);
+                  $(".ph").css("width",w);
+                  $(".footer").css("width",w);
+                  $(".welcome").css("width",w);
+                  $(".siderbar").css("position","static");
+                  $(".main").css("margin-left","0");
+                } 
+                if(w>=768){
+                 $(".text").css("width","910px");
+                  $(".time").css("width","910px");
+                  $(".ph").css("width","910px");
+                  $(".footer").css("width","910px");
+                  $(".welcome").css("width","910px");
+                  $(".main").css("position","absolute");
+                  $(".main").css("margin-left","390px");
+                  $(".siderbar").css("position","fixed");
+               
+                }
+}
+});
